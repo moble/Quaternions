83251bf1ae906948608ee723a522189c4bfc6656
Mike Boyle <boyle@astro.cornell.edu>
2013-10-15 10:21:04 -0400

    Add integrator taking a function and associated tests

--- IntegrateAngularVelocity.cpp
+++ IntegrateAngularVelocity.cpp
@@ -56,6 +56,7 @@ int FrameFromAngularVelocity_RHS(double t, const double ri[], double drdt[], voi
   return GSL_SUCCESS;
 }
 #endif // DOXYGEN
+/// Find the frame with the given angular velocity data
 std::vector<Quaternion> Quaternions::FrameFromAngularVelocity(const std::vector<Quaternion>& Omega, const std::vector<double>& T) {
   ///
   /// \param Omega Vector of Quaternions.
@@ -133,6 +134,98 @@ std::vector<Quaternion> Quaternions::FrameFromAngularVelocity(const std::vector<
   gsl_odeiv2_driver_free(d);
 
   return UnflipRotors(exp(QuaternionArray(rs)));
+}
+
+
+typedef vector<double> (*OmegaFunc)(const double);
+int FrameFromAngularVelocity_RHS_p(double t, const double ri[], double drdt[], void* Omega) {
+  // Pack some values
+  vector<double> rfrak(3);
+  rfrak[0] = ri[0];
+  rfrak[1] = ri[1];
+  rfrak[2] = ri[2];
+  // Evaluate the RHS and unpack
+  const std::vector<double> rfrakDot = Quaternions::FrameFromAngularVelocity_Integrand(rfrak, ((OmegaFunc) Omega)(t));
+  drdt[0] = rfrakDot[0];
+  drdt[1] = rfrakDot[1];
+  drdt[2] = rfrakDot[2];
+  // GSL wants to hear that everything went okay
+  return GSL_SUCCESS;
+}
+/// Find the frame with the given angular velocity function
+void Quaternions::FrameFromAngularVelocity(OmegaFunc Omega, const double t0, const double t1, std::vector<Quaternion>& Qs, std::vector<double>& Ts) {
+  ///
+  /// \param Omega Function pointer returning angular velocity
+  /// \param t0 Initial time
+  /// \param t1 Final time
+  /// \param R0 Initial frame
+  ///
+
+  // Set up the integrator
+  const unsigned int MaxSteps = 10000000; // This is a hard upper limit
+  const double hmin = (t1-t0)/double(MaxSteps);
+  const double hmax = (t1-t0)/100.;
+  const double hstart = (t1-t0)/10000.;
+  const double epsabs = 1.e-12;
+  const double epsrel = 1.e-12;
+  const gsl_odeiv2_step_type* T = gsl_odeiv2_step_rk8pd;
+  gsl_odeiv2_step* s = gsl_odeiv2_step_alloc(T, 3);
+  gsl_odeiv2_control* c = gsl_odeiv2_control_y_new(epsabs, epsrel);
+  gsl_odeiv2_evolve* e = gsl_odeiv2_evolve_alloc(3);
+  gsl_odeiv2_system sys = {FrameFromAngularVelocity_RHS_p, NULL, 3, (void *) Omega};
+  double h = hstart;
+  double t = t0;
+  double r[3] = {0.0, 0.0, 0.0};
+
+  // Run the integration
+  vector<vector<double> > rs;
+  vector<double> ts;
+  rs.push_back(vector<double>(r, r+3));
+  ts.push_back(t);
+  unsigned int NSteps = 0; // Total number of steps
+  unsigned int nSteps = 0; // Number of steps since last change
+  while (t < t1) {
+    // Take a step
+    int status = gsl_odeiv2_evolve_apply(e, c, s, &sys, &t, t+hmax, &h, r);
+    ++NSteps;
+    ++nSteps;
+    if(status != GSL_SUCCESS) {
+      std::cerr << "\n\n" << __FILE__ << ":" << __LINE__ << ": gsl_odeiv2_driver_apply returned an error; return value=" << status << "\n" << std::endl;
+      throw(FailedGSLCall);
+    }
+    rs.push_back(vector<double>(r, r+3));
+    ts.push_back(t);
+
+    // Check if we should stop because there have been too many steps
+    if(NSteps>MaxSteps) {
+      std::cerr << "\n\nThe integration has taken " << NSteps << ".  This seems excessive, so we'll stop." << std::endl;
+      break;
+    }
+
+    // Check if we should stop because the step has gotten too small,
+    // but make sure we at least take 10 steps since the start.
+    if(nSteps>10 && h<hmin) {
+      std::cerr << "Step size " << h << " too small.  Breaking out before we are finished." << std::endl;
+      break;
+    }
+
+    // Reset the value of r if necessary
+    const double absquatlogR = std::sqrt(r[0]*r[0]+r[1]*r[1]+r[2]*r[2]);
+    if(absquatlogR>M_PI/2.) {
+      r[0] = (absquatlogR-M_PI)*r[0]/absquatlogR;
+      r[1] = (absquatlogR-M_PI)*r[1]/absquatlogR;
+      r[2] = (absquatlogR-M_PI)*r[2]/absquatlogR;
+      nSteps=0; // This may make the integrator take a few small steps at first
+    }
+  }
+
+  // Free the gsl storage
+  gsl_odeiv2_evolve_free(e);
+  gsl_odeiv2_control_free(c);
+  gsl_odeiv2_step_free(s);
+
+  Qs = UnflipRotors(exp(QuaternionArray(rs)));
+  Ts = ts;
 }
 
 
---
+++ tests/TestAngularVelocityIntegration.cpp
@@ -0,0 +1,58 @@
+#include <vector>
+#include <iostream>
+#include <iomanip>
+#include "Quaternions.hpp"
+#include "IntegrateAngularVelocity.hpp"
+using namespace std;
+using namespace Quaternions;
+
+const double alpha = 0.05;
+const double T_i = 0.0;
+const double T_f = 20000.0;
+
+Quaternion R_1func(const double t) {
+  return Quaternions::exp(alpha*t*Quaternions::xHat/2.)
+    *Quaternions::exp((alpha*pow(t,1.2))*Quaternions::yHat/2.)
+    *Quaternions::exp((alpha*pow(t,1.3))*Quaternions::zHat/2.);
+}
+
+vector<double> Omega(const double t) {
+  const Quaternion R_1 = R_1func(t);
+  const Quaternion Rdot_1 = (alpha/2.0) *
+    (
+     Quaternions::xHat*
+     Quaternions::exp(alpha*t*Quaternions::xHat/2.)
+     *Quaternions::exp((alpha*pow(t,1.2))*Quaternions::yHat/2.)
+     *Quaternions::exp((alpha*pow(t,1.3))*Quaternions::zHat/2.)
+     +
+     Quaternions::exp(alpha*t*Quaternions::xHat/2.)
+     *1.2*pow(t,0.2)*Quaternions::yHat
+     *Quaternions::exp((alpha*pow(t,1.2))*Quaternions::yHat/2.)
+     *Quaternions::exp((alpha*pow(t,1.3))*Quaternions::zHat/2.)
+     +
+     Quaternions::exp(alpha*t*Quaternions::xHat/2.)
+     *Quaternions::exp((alpha*pow(t,1.2))*Quaternions::yHat/2.)
+     *1.3*pow(t,0.3)*Quaternions::zHat
+     *Quaternions::exp((alpha*pow(t,1.3))*Quaternions::zHat/2.)
+     );
+  return (2*Rdot_1*Quaternions::conjugate(R_1)).vec();
+}
+
+int main() {
+  vector<Quaternion> Qs;
+  vector<double> Ts;
+  cout << setprecision(16);
+  FrameFromAngularVelocity(&Omega, T_i, T_f, Qs, Ts);
+  for(unsigned int i=0; i<Ts.size(); ++i) {
+    cout << Ts[i]
+  	 // << "\n\t" << Qs[i]
+  	 // << "\t" << Qs[i].angle()
+  	 // << "\n\t" << R_1func(Ts[i])
+  	 // << "\n\t" << Qs[i]-R_1func(Ts[i])
+  	 << "\t" << (Qs[i]-R_1func(Ts[i])).abs()
+  	 // << "\t" << 1.e-14*std::sqrt(i)
+  	 << endl;
+  }
+
+  return 0;
+}
